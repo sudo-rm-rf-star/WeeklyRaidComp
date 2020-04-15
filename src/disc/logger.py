edf589f3c20cecfeece3c43a49e9aa3c00679d1c
Jarre Knockaert <jknockae@amazon.com>
2020-04-15 13:44:09 +0100

    Update paths for new project.

--- src/disc/logger.py
+++ src/disc/logger.py
@@ -1,13 +1,14 @@
 import logging
 import logging.handlers
-import os
 from datetime import datetime
+from pathlib import Path
 
-LOG_PATH = os.path.join('data', 'logs', f'{datetime.now().date()}.log')
+LOG_PATH = Path(f'data/logs/{datetime.now().date()}.log')
 
 
 def _setup_logger():
     stdout_handler = logging.StreamHandler()
+    LOG_PATH.parent.mkdir(parents=True, exist_ok=True)
     file_handler = logging.handlers.WatchedFileHandler(LOG_PATH)
     formatter = logging.Formatter(logging.BASIC_FORMAT)
     file_handler.setFormatter(formatter)
