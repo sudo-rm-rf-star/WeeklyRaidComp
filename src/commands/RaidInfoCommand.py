7e92f86d6343c1f1ef4d88bc8657ac7d360be367
jarre <jarre.knockaert@gmail.com>
2020-04-20 22:50:26 +0100

    Argformat is now optional.

--- src/commands/RaidInfoCommand.py
+++ src/commands/RaidInfoCommand.py
@@ -3,7 +3,7 @@ from src.common.Constants import OFFICER_RANK
 
 
 class RaidInfoCommand(BotCommand):
-    def __init__(self, subname, description, argformat):
+    def __init__(self, subname, description, argformat=''):
         allow_trough_approval = False
         required_rank = OFFICER_RANK
         super(RaidInfoCommand, self).__init__('raidinfo', subname, description, argformat, required_rank,
