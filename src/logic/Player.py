a35e4ccdf030f03c242aff73ef306ceac3bb5735
jarre <jarre.knockaert@gmail.com>
2020-05-03 16:18:33 +0100

    DokBotV2

--- src/logic/Player.py
+++ src/logic/Player.py
@@ -1,5 +1,13 @@
+from src.logic.enums.Role import Role
+from src.logic.enums.Class import Class
+from src.logic.enums.Race import Race
+from src.time.DateOptionalTime import DateOptionalTime
+from typing import Dict, Set, Optional
+
+
 class Player:
-    def __init__(self, discord_id, char_name, klass, role, race, present_dates=None, standby_dates=None):
+    def __init__(self, discord_id: int, char_name: str, klass: Class, role: Role, race: Race, present_dates: Optional[Dict[str, Set[DateOptionalTime]]] = None,
+                 standby_dates: Optional[Dict[str, Set[DateOptionalTime]]] = None):
         self.discord_id = discord_id
         self.name = char_name
         self.klass = klass
@@ -9,25 +17,24 @@ class Player:
         self.present_dates = {} if not present_dates else present_dates
         self.standby_dates = {} if not standby_dates else standby_dates
 
-    def add_standby_date(self, raid_name, raid_datetime):
+    def add_standby_date(self, raid_name: str, raid_datetime: DateOptionalTime):
         if raid_name not in self.standby_dates:
             self.standby_dates[raid_name] = set()
         self.standby_dates[raid_name].add(raid_datetime)
 
-    def add_present_date(self, raid_name, raid_datetime):
+    def add_present_date(self, raid_name: str, raid_datetime: DateOptionalTime):
         if raid_name not in self.present_dates:
             self.present_dates[raid_name] = set()
         self.present_dates[raid_name].add(raid_datetime)
 
-    def get_standby_dates(self, raid_name):
+    def get_standby_dates(self, raid_name: str) -> Set[DateOptionalTime]:
         return self.standby_dates.get(raid_name, set())
 
-    def get_standby_count(self, raid_name):
+    def get_standby_count(self, raid_name: str) -> int:
         return len(self.get_standby_dates(raid_name))
 
-    def __eq__(self, other):
+    def __eq__(self, other) -> bool:
         return self.name == other.name
 
-    def __str__(self):
-        return f'{self.role} {self.klass} {self.name}'
-
+    def __str__(self) -> str:
+        return f'{self.role.name.capitalize()} {self.klass.name.capitalize()} {self.name}'
