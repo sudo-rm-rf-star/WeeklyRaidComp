6724664038e14e88134f4491415141b1bf4476fe
jarre <jarre.knockaert@gmail.com>
2020-05-04 13:21:43 +0100

    Handle HTTPException

--- src/client/entities/DiscordMessage.py
+++ src/client/entities/DiscordMessage.py
@@ -1,4 +1,5 @@
 import discord
+import src.client.Logger as Log
 from typing import Union
 
 
@@ -8,4 +9,10 @@ class DiscordMessage:
         self.embed = embed
 
     async def send_to(self, recipient: Union[discord.Member, discord.TextChannel]) -> discord.Message:
-        return await recipient.send(content=self.content, embed=self.embed)
+        try:
+            return await recipient.send(content=self.content, embed=self.embed)
+        except discord.HTTPException as e:
+            Log.error(f'Failed to send following message to {recipient}: content {self.content}, embed: {self.embed.to_dict()}')
+            raise e
+
+
