18f224d2665b26570a7e7f680e687af3e0baa29a
jarre <jarre.knockaert@gmail.com>
2020-05-05 22:44:18 +0100

    Repair data raids

--- src/extra/repair_data.py
+++ src/extra/repair_data.py
@@ -9,11 +9,11 @@ def repair():
             print(player_name, signee_choice.value)
             if isinstance(signee_choice.value, tuple):
                 print(f'Updated {player_name} with {SignupStatus[signee_choice.name]}')
-                event.rosters.signee_choices[player_name] = SignupStatus[signee_choice.name]
+                # event.rosters.signee_choices[player_name] = SignupStatus[signee_choice.name]
         for i in range(len(event.rosters.rosters)):
             for player_name, roster_choice in event.rosters.rosters[i].roster_choices.items():
                 print(player_name, roster_choice.value)
                 if isinstance(roster_choice.value, tuple):
                     print(f'Updated {player_name} with {RosterStatus[roster_choice.name]}')
-                    event.rosters.rosters[i].roster_choices[player_name] = RosterStatus[roster_choice.name]
+                    # event.rosters.rosters[i].roster_choices[player_name] = RosterStatus[roster_choice.name]
     RaidEvents().store()
