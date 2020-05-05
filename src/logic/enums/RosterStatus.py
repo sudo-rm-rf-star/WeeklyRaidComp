168b40bb98c348d2ad4f50e597ad3a36d181f7f2
jarre <jarre.knockaert@gmail.com>
2020-05-05 22:10:32 +0100

    Repair data raids

--- src/logic/enums/RosterStatus.py
+++ src/logic/enums/RosterStatus.py
@@ -5,7 +5,7 @@ class RosterStatus(Enum):
     """ The number represents the priority a person gets in the raid depending on his roster state.
     A lower number equals a higher priority. This is only used when updating the roster which already has decided players.
     Players initially accepted get a higher priority"""
-    ACCEPT = 1,
-    EXTRA = 3,
-    UNDECIDED = 2,
+    ACCEPT = 1
+    UNDECIDED = 2
+    EXTRA = 3
     DECLINE = 4
