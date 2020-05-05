d6d99074a8caeb06ebe084c815c58727048a7529
jarre <jarre.knockaert@gmail.com>
2020-05-05 22:20:06 +0100

    Repair data raids

--- src/extra/repair_data.py
+++ src/extra/repair_data.py
@@ -1,16 +1,18 @@
 from src.logic.RaidEvents import RaidEvents
+from src.logic.enums.RosterStatus2 import RosterStatus
+from src.logic.enums.SignupStatus2 import SignupStatus
 
 def repair():
     for event in RaidEvents().all():
         for player_name, signee_choice in event.rosters.signee_choices.items():
             print(player_name, signee_choice.value)
             if isinstance(signee_choice.value, tuple):
-                event.rosters.signee_choices[player_name].value = signee_choice.value[0]
+                event.rosters.signee_choices[player_name] = SignupStatus[signee_choice.name]
         for i in range(len(event.rosters.rosters)):
             for player_name, roster_choice in event.rosters.rosters[i].roster_choices.items():
                 print(player_name, roster_choice)
                 if isinstance(roster_choice.value, tuple):
-                    event.rosters.rosters[i].roster_choices[player_name].value = roster_choice.value[0]
+                    event.rosters.rosters[i].roster_choices[player_name] = RosterStatus[roster_choice.name]
     RaidEvents().store()
 
 
