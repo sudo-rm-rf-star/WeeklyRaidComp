63d54996c2198205ff1887712e82ea67c111494e
jarre <jarre.knockaert@gmail.com>
2020-05-05 22:14:09 +0100

    Repair data raids

--- src/extra/repair_data.py
+++ src/extra/repair_data.py
@@ -3,10 +3,12 @@ from src.logic.RaidEvents import RaidEvents
 def repair():
     for event in RaidEvents().all():
         for player_name, signee_choice in event.rosters.signee_choices.items():
+            print(player_name, signee_choice)
             if isinstance(signee_choice, tuple):
                 event.rosters.signee_choices[player_name] = signee_choice[0]
         for i in range(len(event.rosters.rosters)):
             for player_name, roster_choice in event.rosters.rosters[i].roster_choices.items():
+                print(player_name, roster_choice)
                 if isinstance(roster_choice, tuple):
                     event.rosters.rosters[i].roster_choices[player_name] = roster_choice[0]
     RaidEvents().store()
