95094afce64f7eb719fea1f16e83192d5e3455d3
jarre <jarre.knockaert@gmail.com>
2020-05-05 22:33:21 +0100

    Repair data raids

---
+++ src/logic/enums/RosterStatus2.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class RosterStatus(Enum):
+    """ The number represents the priority a person gets in the raid depending on his roster state.
+    A lower number equals a higher priority. This is only used when updating the roster which already has decided players.
+    Players initially accepted get a higher priority"""
+    ACCEPT = 1
+    EXTRA = 3
+    UNDECIDED = 2
+    DECLINE = 4
---
+++ src/logic/enums/SignupStatus2.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+
+class SignupStatus(Enum):
+    """ The number represents the priority a person gets in the raid depending on his signup state.
+    A lower number equals a higher priority."""
+    ACCEPT = 1
+    UNDECIDED = 2
+    LATE = 3
+    BENCH = 4
+    TENTATIVE = 5
+    DECLINE = 6
+
+    def __lt__(self, other):
+        return self.value < other.value
