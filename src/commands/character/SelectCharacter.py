d1b011f27a9e8d6ec39ad152e41f00309afe658a
jarre <jarre.knockaert@gmail.com>
2020-05-23 23:11:09 +0100

    Support multiple characters

--- src/commands/character/SelectCharacter.py
+++ src/commands/character/SelectCharacter.py
@@ -12,7 +12,8 @@ class SelectCharacter(CharacterCommand):
     async def execute(self, player: str, **kwargs) -> None:
         success = self.players_resource.select_character(player)
         if success:
-            self.respond(f'You will now sign up as {player} for raids')
+            self.respond(f'You will now sign up as {player} for raids. '
+                         f'If you want to change the character with which you signed, please sign again for that raid.')
         else:
             self.respond(f'We could not find your character {player}. Perhaps you need to add it with the add '
                          f'character command')
