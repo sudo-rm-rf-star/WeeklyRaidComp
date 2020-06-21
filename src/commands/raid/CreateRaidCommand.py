438cd6dfea8b522f901dd5de918ed4ec4249f763
Jarre <jarre.knockaert@gmail.com>
2020-06-21 19:51:08 +0200

    Update for prod

--- src/commands/raid/CreateRaidCommand.py
+++ src/commands/raid/CreateRaidCommand.py
@@ -14,8 +14,7 @@ class CreateRaidCommand(RaidCommand):
     def description(cls) -> str: return "Create a new event for a raid"
 
     async def execute(self, raid_name, raid_datetime, **kwargs):
-        # raiders = self.get_raiders()
-        raiders = [await get_member_by_id(self.discord_guild, MAINTAINER_ID), await get_member_by_id(self.discord_guild, TESTER_ID)]
+        raiders = self.get_raiders()
         events_channel = await self.get_events_channel()
         response = await self.events_resource.create_raid(discord_guild=self.discord_guild, group_id=self._raidgroup.group_id, raid_name=raid_name,
                                                           raid_datetime=raid_datetime, raiders=raiders, events_channel=events_channel)
