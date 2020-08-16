62f040fc2f0289b59d3f00b2abc771675e868292
jarre <jarre.knockaert@gmail.com>
2020-08-16 20:50:20 +0200

    Minor QoL changes

--- src/commands/raid/RemoveRaidEvent.py
+++ src/commands/raid/RemoveRaidEvent.py
@@ -5,21 +5,23 @@ from utils.DiscordUtils import get_member_by_id
 
 class RemoveRaidCommand(RaidCommand):
     @classmethod
-    def subname(cls) -> str: return "remove"
+    def subname(cls) -> str:
+        return "remove"
 
     @classmethod
-    def argformat(cls) -> str: return "raid_name raid_date raid_time"
+    def argformat(cls) -> str:
+        return "raid_name [raid_date][raid_time]"
 
     @classmethod
-    def description(cls) -> str: return "Remove the event for a raid"
+    def description(cls) -> str:
+        return "Remove the event for a raid"
 
     async def execute(self, raid_name: str, raid_datetime: DateOptionalTime, **kwargs):
         raid_event = self.get_raid_event(raid_name, raid_datetime)
         await self.events_resource.remove_raid(self.discord_guild, raid_event)
         for player in raid_event.get_signed_characters():
-            member = get_member_by_id(self.discord_guild, player.discord_id)
+            member = await get_member_by_id(self.discord_guild, player.discord_id)
             if member is not None:
                 member.send(f'{raid_event} has been cancelled. Thanks for signing up!')
-
 
         self.respond(f'{raid_event} has been successfully deleted.')
