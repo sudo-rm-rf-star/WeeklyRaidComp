d22aa26c9b820add331079a2486db132eba7d280
Jarre Knockaert <jarre@edgardcooper.com>
2021-01-23 22:30:53 +0100

    WebApp v1

--- application.py
+++ application.py
@@ -4,13 +4,11 @@ from flask import Flask
 from src.webapp.flask import run
 
 # EB looks for an 'application' callable by default.
-application = Flask(__name__)
+application = Flask(__name__, root_path=Path(__file__).parent / 'src')
 
 # run the app.
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
-    cwd = Path(__file__).parent / 'src'
-    sys.path.append(str(cwd))
     application.debug = True
     run(application)
