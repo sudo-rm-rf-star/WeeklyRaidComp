a1a698e86ec8ee5fa39ad0c238a14f80d06f9797
Jarre Knockaert <jarre@edgardcooper.com>
2021-01-27 01:05:31 +0100

    Update raid event pages

--- src/webapp/controllers/RaidController.py
+++ src/webapp/controllers/RaidController.py
@@ -26,7 +26,7 @@ class RaidController(AbstractController):
     def show(self, name, timestamp):
         dt = DateOptionalTime.from_timestamp(timestamp)
         raid = self.events_table.get_raid_event(self.guild_id, self.group_id, name, dt)
-        return self.view('show', raid=raid)
+        return self.view('show', raid=raid, player=self.player)
 
     def store(self, form):
         raid_datetime = datetime.strptime(f"{form['date']} {form['time']}", "%Y-%m-%d %H:%M")
