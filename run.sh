db7295f002394fa56e956685a3c837d9b99c5cf7
Jarre Knockaert <jarre@edgardcooper.com>
2021-07-14 10:30:20 +0200

    Use pip through python

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 git pull
 git checkout production
 git reset --hard origin/production
-pip3 install -r requirements.txt
+python3 -m pip install -r requirements.txt
 mkdir -p logs
 python3 bot.py &>> logs/dokbot-$(date +%d-%m-%Y).log &
