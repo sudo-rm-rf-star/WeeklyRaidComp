4f90afa2ec5fecd0c6ad33cb56b8bc8285b45560
Jarre Knockaert <jarre@edgardcooper.com>
2021-07-14 10:40:39 +0200

    Improve run script

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
+CUR_DIR=$(pwd)
+cd "$(dirname "$0")"
 git pull
 git checkout production
 git reset --hard origin/production
 python3 -m pip install -r requirements.txt
 mkdir -p logs
 python3 bot.py &>> logs/dokbot-$(date +%d-%m-%Y).log &
+cd $CUR_DIR
