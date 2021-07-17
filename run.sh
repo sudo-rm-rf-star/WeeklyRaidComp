6706916baba396fe2a4be3a35dbab2f29fdaa119
Yannick Motton <ymotton@gmail.com>
2021-07-17 23:00:26 +0200

    Wraps dokbot into docker container with docker-compose

--- run.sh
+++ run.sh
@@ -4,7 +4,6 @@ cd "$(dirname "$0")"
 git pull
 git checkout production
 git reset --hard origin/production
-python3 -m pip install -r requirements.txt
-mkdir -p logs
-python3 bot.py &>> logs/dokbot-$(date +%d-%m-%Y).log &
+docker-compose build
+docker-compose up -d
 cd $CUR_DIR
