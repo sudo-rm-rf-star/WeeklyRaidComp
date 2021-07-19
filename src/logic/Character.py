79f679b9fbea9588efeff151c95e681ba9e0b559
Jarre Knockaert <jarre@edgardcooper.com>
2021-07-19 18:00:18 +0200

    Add optional third argument to roster status depicting team index

--- src/logic/Character.py
+++ src/logic/Character.py
@@ -8,10 +8,23 @@ from logic.enums.SignupStatus import SignupStatus
 from typing import List, Tuple
 
 
+def map_roster_statuses(roster_statuses: List[Tuple]):
+    """
+    Utility function to add optional team index to roster status
+    """
+    for roster_status in roster_statuses:
+        if len(roster_status) == 2:
+            signup_status, timestamp = roster_status
+            team_index = 0
+        else:
+            signup_status, timestamp, team_index = roster_status
+        yield signup_status, timestamp, team_index
+
+
 class Character:
     def __init__(self, *, char_name: str, klass: Class, spec: str, discord_id: int,
                  created_at: Optional[float] = None,
-                 roster_statuses: Optional[List[Tuple[RosterStatus, int]]] = None,
+                 roster_statuses: Optional[List[Tuple[RosterStatus, int, int]]] = None,
                  signup_statuses: Optional[List[Tuple[SignupStatus, int]]] = None,
                  realm: str = None, region: str = None):
         self.name = char_name
@@ -27,8 +40,8 @@ class Character:
     def set_signup_status(self, signup_status: SignupStatus):
         self.signup_statuses.append((signup_status, int(datetime.now().timestamp())))
 
-    def set_roster_status(self, roster_status: RosterStatus):
-        self.roster_statuses.append((roster_status, int(datetime.now().timestamp())))
+    def set_roster_status(self, roster_status: RosterStatus, team_id: int = 0):
+        self.roster_statuses.append((roster_status, int(datetime.now().timestamp()), team_id))
 
     def get_signup_status(self) -> SignupStatus:
         if len(self.signup_statuses) > 0:
@@ -56,8 +69,9 @@ class Character:
                          discord_id=item.get('discord_id', None),
                          signup_statuses=[(SignupStatus[signup_status], timestamp) for
                                           signup_status, timestamp in item.get('signup_statuses', [])],
-                         roster_statuses=[(RosterStatus[roster_status], timestamp) for
-                                          roster_status, timestamp in item.get('roster_statuses', [])],
+                         roster_statuses=[(RosterStatus[roster_status], timestamp, team_index) for
+                                          roster_status, timestamp, team_index in
+                                          map_roster_statuses(item.get('roster_statuses', []))],
                          created_at=item.get('created_at', None))
 
     def to_dict(self) -> Dict[str, Any]:
@@ -66,8 +80,10 @@ class Character:
             'discord_id': int(self.discord_id),
             'class': self.klass.name,
             'spec': self.spec,
-            'roster_statuses': [(roster_status.name, int(timestamp)) for roster_status, timestamp in self.roster_statuses],
-            'signup_statuses': [(signup_status.name, int(timestamp)) for signup_status, timestamp in self.signup_statuses],
+            'roster_statuses': [(roster_status.name, int(timestamp), team_index) for
+                                roster_status, timestamp, team_index in self.roster_statuses],
+            'signup_statuses': [(signup_status.name, int(timestamp)) for signup_status, timestamp in
+                                self.signup_statuses],
             'created_at': int(self.created_at if self.created_at else datetime.now().timestamp())
         }
 
